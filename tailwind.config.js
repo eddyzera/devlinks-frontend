2fb364df3be132fc7caf4e6cc32b134f3459ab1a
eddyzera <esilva644@gmail.com>
2023-07-24 19:23:21 -0300

    style: extends my theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,35 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'dark-purple': 'var(--purple-900)',
+        'middle-purple': 'var(--purple-200)',
+        'light-purple': 'var(--purple-100)',
+        'dark-gray': 'var(--gray-900)',
+        'middle-gray': 'var(--gray-800)',
+        'light-gray': 'var(--gray-500)',
+        gray: 'var(--gray-100)',
+        white: 'var(--white)',
+        'dark-red': 'var(--red-900)',
+      },
+      borderRadius: {
+        'radius-md': 'var(--radius-md)',
+      },
+      boxShadow: {
+        'shadow-md': 'var(--shadow-md)',
+      },
+      fontSize: {
+        'text-12': 'var(--text-sm)',
+        'text-16': 'var(--text-md)',
+        'text-32': 'var(--text-lg)',
+      },
+      lineHeight: {
+        'line-150': 'var(--line-height)',
+      },
+
+      maxWidth: {
+        'max-w-482': 'var(--max-w-sm)',
+        'max-w-546': 'var(--max-w-md)',
       },
     },
   },
