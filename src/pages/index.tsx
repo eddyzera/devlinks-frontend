1f230f168a151edeb9d1e7d1210117bcf3401fa6
eddyzera <esilva644@gmail.com>
2023-07-28 17:26:47 -0300

    feat: login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
+import { Card } from '@/components/Card'
+import { Form } from '@/components/Form'
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-dark-purple">hello world</h1>
-    </div>
+    <section className="h-screen w-full max-w-max-w-482 m-auto flex items-center justify-center">
+      <Card.Root className="w-full">
+        <Form.Root>
+          <div className="mb-10">
+            <h2 className="text-dark-gray text-text-32 font-bold leading-line-150">
+              Login
+            </h2>
+            <p className="text-middle-gray">
+              Add your details below to get back into the app
+            </p>
+          </div>
+          <div>input</div>
+        </Form.Root>
+      </Card.Root>
+    </section>
   )
 }
